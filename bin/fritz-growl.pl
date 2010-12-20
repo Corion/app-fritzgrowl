469bc280d0d700f9dfcc7982e0a5998861be464b
Max Maischein <corion@corion.net>
2010-12-20 20:28:20 +0100

    Add filter to only send matching numbers to Growl

--- bin/fritz-growl.pl
+++ bin/fritz-growl.pl
@@ -4,18 +4,34 @@ use AnyEvent;
 use AnyEvent::FritzBox;
 use Data::Dumper;
 use Growl::Any;
+use Getopt::Long;
 
-#my $growl = Growl::Any->new;
-#$growl->register("FritzGrowl", ['Incoming call']);
+my $growl = Growl::Any->new;
+$growl->register("FritzGrowl", ['Incoming call']);
+
+GetOptions(
+    "filter|f:s" => \my $local_filter,
+);
+if (! defined $local_filter) {
+    $local_filter = '';
+};
+$local_filter = qr/$local_filter/;
 
 my $fb = AnyEvent::FritzBox->new(
     host => '192.168.1.104',
     on_ring => sub {
         my ($fb,%args) = @_;
-        #print "RING: $args{remote_number}\n";
-        #print Dumper \%args;
-        my $clearname = "Unknown";
-        #$growl->notify("Incoming call", $args{ remote_number }, $clearname);
+        if ($args{ local_number } =~ /$local_filter/) {
+            print "RING: $args{remote_number}\n";
+            #print Dumper \%args;
+            
+            # Do the reverse lookup here to find a name
+            # Also, have a timeout here - if we don't find the name after 2 seconds
+            # (maybe due to connectivity problems), just display it as unknown
+            
+            my $clearname = "Unknown";
+            $growl->notify("Incoming call", $args{ remote_number }, $clearname);
+        };
     },
     on_call => sub {
         my ($fb,%args) = @_;
@@ -23,6 +39,5 @@ my $fb = AnyEvent::FritzBox->new(
         #print Dumper \%args;
     },
 );
-
 
 AnyEvent->condvar->recv;
